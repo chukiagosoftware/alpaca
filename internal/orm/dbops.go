e763cb65d9522c52c6e5ebbfee79d29d17bf70dc
Eric Arellano <10988120+edam-software@users.noreply.github.com>
2026-03-24 20:55:11 -0400

    Improvements and temp changes to internal dbops functions for Google/TripAdvisor fetches

--- internal/orm/dbops.go
+++ internal/orm/dbops.go
@@ -13,8 +13,11 @@ import (
 )
 
 // GetAllHotels retrieves all hotels from the database
-func (db *DB) GetAllHotels(ctx context.Context) ([]*models.Hotel, error) {
+func (db *DB) GetAllHotels(ctx context.Context, source string) ([]*models.Hotel, error) {
 	var hotels []*models.Hotel
+	if source != "" {
+		return hotels, db.DB.WithContext(ctx).Where("source = ? and hotel_id > 'ta_1161758'", source).Order("hotel_id").Find(&hotels).Error
+	}
 	return hotels, db.DB.WithContext(ctx).Order("hotel_id").Find(&hotels).Error
 }
 
